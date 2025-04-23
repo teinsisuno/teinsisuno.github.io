9153469358435b5dafccbd3890caf0a0bdb1b4b8
teinsisuno <tein.bejo1@gmail.com>
2025-04-23 21:37:11 +0700

    Inisialisasi ulang repository

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import headerTheme from './theme/headerTheme.vue'
+</script>
+
+<template>
+  <header-theme />
+  <main class="w-full bg-orange-500 h-screen rounded-t-[50%]">
+    <div class="max-w-5xl mx-auto pt-40 px-8">
+      <h1 class="text-4xl font-bold text-center text-white">Hello saya tengku</h1>
+      <p class="text-center text-white">Ini cikal bakal website saya</p>
+      <p class="text-center text-white">ditunggu kelanjutannya</p>
+      <p class="text-center text-white">test</p>
+    </div>
+  </main>
+</template>
+
+<style scoped> </style>
---
+++ src/theme/headerTheme.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="fixed top-6 left-1/2 -translate-x-1/2 bg-gray-800 text-white w-5xl px-12 py-6 mt-8 rounded-full">
+
+        <div class="flex justify-between items-center">
+            <div class="flex items-center">
+                <h1 class="text-2xl font-bold leading-none">Teinsisuno</h1>
+            </div>
+            <nav>
+                <ul class="flex justify-between items-center space-x-4">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    
+                </ul>
+            </nav>
+        </div>
+            
+
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
+
